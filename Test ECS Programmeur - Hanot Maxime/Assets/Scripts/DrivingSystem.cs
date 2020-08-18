4d1e2e96469f63b51cef840efd5e238bb6e8674f
MaximeHanot <maxime.hanot@pole3d.com>
2020-08-18 18:28:20 +0200

    Ajout du projet

---
+++ Test ECS Programmeur - Hanot Maxime/Assets/Scripts/DrivingSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEditor;
+using UnityEngine;
+//using Unity.Transforms;
+
+public struct Driving : IComponentData
+{
+    public float speed;
+
+    public List<Vector3> path;
+
+    //public bool frontDetection = false;
+
+
+}
+
+public class CarEntity : MonoBehaviour
+{
+    private void Start()
+    {
+        EntityManager m = World.Active.EntityManager;
+        Entity e = m.CreateEntity();
+        m.AddComponent<Driving>(e);
+
+        Driving drivingData = m.GetComponentData<Driving>(e);
+
+        drivingData.speed = Random.Range(0, 10);
+
+        m.SetComponentData<Driving>(e, drivingData);
+        
+    }
+}
+
+public class DrivingSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        EntityManager m = World.Active.EntityManager;
+        Entities.ForEach((Entity entity, ref Driving _driving, Transform _transform) => 
+        {
+            _driving.speed = 4;
+        });
+    }
+}
---
+++ Test ECS Programmeur - Hanot Maxime/Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //public int roadNumbers;
+    public Vector2 roadLengths;
+    GameObject ground;
+    [Serializable]
+    public struct road
+    {
+        public List<Vector3> roadPoints;
+    }
+
+    public List<road> roads;
+
+    private void Awake()
+    {
+        ground = GameObject.FindGameObjectWithTag("Ground");
+    }
+    private void Start()
+    {
+        for(int i = 0; i < roads.Count; i++)
+        {
+            GenerateRoad(i);
+        }
+    }
+
+    void GenerateFirstPoint(int i)
+    {
+        //set first point coordinates in ground dimensions and add to list
+        float xPosition = UnityEngine.Random.Range(0.2f, ground.transform.localScale.x);
+        float zPosition = UnityEngine.Random.Range(0.2f, ground.transform.localScale.z);
+        roads[i].roadPoints.Add( new Vector3(xPosition, 0, zPosition) ) ;
+    }
+
+    void GenerateRoad(int i)
+    {
+        GenerateFirstPoint(i);
+
+        //set road length by random between two int
+        int rdmLength = UnityEngine.Random.Range((int)roadLengths.x, (int)roadLengths.y);
+        //Debug.Log(rdmLength);
+
+        //set second point to determine a direction for the road
+        float xPosition = roads[i].roadPoints[0].x + UnityEngine.Random.Range(-1f, 1f);
+        float zPosition = roads[i].roadPoints[0].z + UnityEngine.Random.Range(-1f, 1f);
+
+        //add the second point in the list
+        roads[i].roadPoints.Add(new Vector3(xPosition, 0, zPosition));
+        
+
+        //calulate direction for others points
+        Vector3 dir;
+        if (roads[i].roadPoints.Count >= 2)
+            dir = roads[i].roadPoints[1] - roads[i].roadPoints[0];
+        else
+            dir = Vector3.zero;
+
+        //add new point while the list count is less than the random length
+        while (roads[i].roadPoints.Count < rdmLength - 1)
+        {
+            float xPositionNext = roads[i].roadPoints[roads[i].roadPoints.Count - 1].x + UnityEngine.Random.Range(-.5f, .5f);
+            float zPositionNext = roads[i].roadPoints[roads[i].roadPoints.Count - 1].z + UnityEngine.Random.Range(-.5f, .5f);
+            roads[i].roadPoints.Add(new Vector3(xPositionNext, 0, zPositionNext) + dir);
+        }
+
+        //Draw a ray to show the road on the scene
+        for(int k = 2; k < roads[i].roadPoints.Count; k++)
+        {
+            Debug.DrawRay(roads[i].roadPoints[k-1], roads[i].roadPoints[k-2] - roads[i].roadPoints[k - 1], Color.blue,Mathf.Infinity);
+        }
+    }
+}
